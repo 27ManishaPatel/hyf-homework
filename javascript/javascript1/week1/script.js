8fc0fa1ce9d6e02dfa7e696731977b36d6e2f05e
27ManishaPatel <manisha270989@gmail.com>
2022-05-23 22:54:58 +0200

    age-ify calculator

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,1 +1,9 @@
 console.log("Hello");
+
+const yearOfBirth = 1989;
+const yearFuture = 2050;
+
+const age  = yearFuture  - yearOfBirth;
+
+console.log("I will be " + age + " years old in " + yearFuture);
+
