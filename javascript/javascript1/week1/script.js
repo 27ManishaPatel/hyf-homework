66fa23ca5c3663243f36d454a6df6c6fa4beb549
27ManishaPatel <manisha270989@gmail.com>
2022-05-24 10:04:43 +0200

    3rd exercise Housey pricey

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -24,4 +24,45 @@ console.log("I will be " + myAge + " years old in " + yearFuture);
      console.log("My dog will be " + dogHumunsYear+ " humuns years old in " + dogYearFuture )
  };
 
+ //Housey pricey (A house price estimator)
+
+//For Peter house
+const gardenSizeInM2 = 100 ;
+const phWidth = 8;
+const phHeight = 10;
+const phDepth = 10;
+//lets calculate volumeInMeters
+let phvolumeInMeters = phWidth * phHeight * phDepth;
+//What peter is paying now 
+const actualPetersRent = 2500000;
+
+housePriceforpeter = phvolumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+
+if (housePriceforpeter < actualPetersRent){
+    console.log("Peter is paying too much")
+} else if (housePriceforpeter == actualPetersRent){
+   console.log("Peter is paying expected amount")
+} else {
+   console.log("Peter is paying less")
+};
+
+//For Julia house
+const jhgardenSizeInM2 = 70 ;
+const jhWidth = 5;
+const jhHeight = 8;
+const jhDepth = 11;
+//lets calculate volumeInMeters
+let jhvolumeInMeters = jhWidth * jhHeight * jhDepth;
+//What peter is paying now 
+const actualJuliasRent = 1000000;
+
+housePriceforJulia = jhvolumeInMeters  * 2.5 * 1000 + jhgardenSizeInM2 * 300;
+
+if (housePriceforJulia < actualJuliasRent){
+    console.log("Julia is paying too much")
+} else if (housePriceforJulia == actualJuliasRent){
+   console.log("Julia is paying expected amount")
+} else {
+   console.log("Julia is paying less")
+};
 
