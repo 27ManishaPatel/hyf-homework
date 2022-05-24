16b992fd5f63353f54c8f3d24f6d5946210caacc
27ManishaPatel <manisha270989@gmail.com>
2022-05-24 10:02:17 +0200

    2nd exercise Goodboy-Oldboy

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -1,9 +1,27 @@
-console.log("Hello");
+console.log("Hello"); //Initial code
 
+//Age-ify (A future age calculator)
 const yearOfBirth = 1989;
 const yearFuture = 2050;
 
-const age  = yearFuture  - yearOfBirth;
+//with using - arithmatic operator we can calculate the age.
+let myAge  = yearFuture  - yearOfBirth;
 
-console.log("I will be " + age + " years old in " + yearFuture);
+console.log("I will be " + myAge + " years old in " + yearFuture);
+
+
+//Goodboy-Oldboy (A dog age calculator)
+ const dogYearOfBirth = 2017;
+ const dogYearFuture = 2027;
+ const dogYear = dogYearFuture - dogYearOfBirth;
+ const dogHumunsYear = 7 * dogYear ;
+ let shouldShowResultInDogYears = "true";
+
+
+ if (shouldShowResultInDogYears == "true") {
+     console.log("My dog will be " + dogYear+ " dog years old in " + dogYearFuture )
+ } else {
+     console.log("My dog will be " + dogHumunsYear+ " humuns years old in " + dogYearFuture )
+ };
+
 
