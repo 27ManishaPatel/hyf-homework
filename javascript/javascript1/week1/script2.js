ada6b3dc730ae747bfe1d12821110b928361b12f
27ManishaPatel <manisha270989@gmail.com>
2022-05-25 00:03:54 +0200

    called a function for dogs age calculator

--- javascript/javascript1/week1/script2.js
+++ javascript/javascript1/week1/script2.js
@@ -8,3 +8,18 @@ function futureAge() {
 
     document.getElementById("calculatedage").value = "I will be " + futureAge + " years old in " + yearFuture;
 }
+
+// call the function to calculate Dog's future age:
+
+function dogFutureAge() {
+    let dogsYearOfBirth = document.getElementById("dogbirthyear").value;
+    let dogsYearFuture = document.getElementById("dogfutureyear").value;
+
+    const dogsFutureAge = dogsYearFuture - dogsYearOfBirth ;
+
+    document.getElementById("dogcalculatedage").value = "My dog will be " + dogsFutureAge + " dog years old in " + dogYearFuture;
+
+    const dogsHumunFutureAge = dogsFutureAge * 7 ;
+    document.getElementById("dogcalculatedhumanage").value = "My dog will be " + dogsHumunFutureAge + " dog years old in " + dogYearFuture;
+
+}
