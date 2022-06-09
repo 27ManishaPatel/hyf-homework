fe318e9f229e14a9a66e987ca80db7edcd81cb25
27ManishaPatel <manisha270989@gmail.com>
2022-06-09 09:58:31 +0200

    CactusIO-interactive (Smart phone usage app) optional

--- javascript/javascript1/week3/script2.js
+++ javascript/javascript1/week3/script2.js
@@ -47,10 +47,22 @@ logOutNotesFormatted(); // The note with id: 1, has the following note text: Pic
 //CactusIO-interactive (Smart phone usage app) optional
 
 // Adding an activity
+//added date format
+let today = new Date();
+const options = { weekday: 'long', month: 'short', day: 'numeric', year: '2-digit', };
+Date = today.toLocaleDateString("en-US", options);
+
+// const options = { weekday: 'narrow', year: '2-digit', month: '2-digit', day: 'numeric' };
+// let today = new Date();
+// date = today.toLocaleDateString(undefined, options);
+console.log(Date);
 
 let activities = [];
 function addActivity(Date, Activity, Duration) {
-    return activities.push({ Date, Activity, Duration });
+    if (typeof Date == 'string' && typeof Activity == 'string' && typeof Duration == 'number') {
+        return activities.push({ Date, Activity, Duration });
+    }
+
 }
 
 addActivity("23/7-18", "Youtube", 30);
@@ -69,7 +81,7 @@ function showStatus(activities) {
 
     if (activities.length == 0) {
         return console.log("Add some activities before calling showStatus");
-    } else if (sum < 200) {
+    } else if (sum < 200) {  //Usage limit
         return console.log("You have added " + activities.length + " activities They amount to " + sum + " min. of usage");
     } else {
         return console.log("You have reached your limit, no more smartphoning for you!");
@@ -77,5 +89,15 @@ function showStatus(activities) {
 
 }
 showStatus(activities);
-
-//Usage limit
+//Create a function for calculating the activity a user has spent the most time on.
+function spentMostTime(activities) {
+    let mostSpentTime = 0;
+    for (let i = 0; i < activities.length; i++) {
+        if (activities[i].Duration > mostSpentTime) {
+            mostSpentTime = activities[i].Duration;
+            mostSpentActivity = activities[i].Activity;
+        }
+    }
+    return console.log("You have spent most of the time (around " + mostSpentTime + " min) 0n " + mostSpentActivity);
+}
+spentMostTime(activities); //"You have spent most of the time (around 50 min) 0n Facebook"
