83faae6a2cec1fd1353e1fdf19b4006ae1c7d3ec
HackYourFuture DK <manisha270989@gmail.com>
2022-08-09 11:07:41 +0200

    github repos

--- javascript/javascript3/week3/githubRepo/script.js
+++ javascript/javascript3/week3/githubRepo/script.js
@@ -1,21 +1,29 @@
 function toGetUrlRepo(userName) {
-    const user = fetch(`https://api.github.com/search/repositories?q=user:${userName}`)
-    user.then(Response => Response.json())
-        .then(data => {
-            console.log(data)
-            const ul = document.createElement("ul");
-            ul.innerHTML = userName;
-            ul.className = "user-name"
-            document.body.appendChild(ul)
+  const user = fetch(
+    `https://api.github.com/search/repositories?q=user:${userName}`,
+  )
+  user
+    .then((Response) => Response.json())
+    .then((data) => {
+      console.log(data)
+      const ul = document.createElement('ul')
+      ul.innerHTML = `${userName} 's Repositories`
+      ul.className = 'user-name'
+      document.body.appendChild(ul)
 
-            const repo = data.items.forEach(name => {
-                const li = document.createElement("li");
-                li.innerHTML = `${name.name} : https://github.com/${name.full_name}`;
-                li.className = "users-repo-url"
-                ul.appendChild(li)
-            });
-        })
+      const repo = data.items.forEach((name) => {
+        const li = document.createElement('li')
+       li.innerHTML = `${name.name} : ${name.git_url}`
+        li.className = 'users-repo-url'
+        ul.appendChild(li);
+      })
+    })
 }
-toGetUrlRepo("Hailegebriel22");
-toGetUrlRepo("patel-prakashkumar");
-toGetUrlRepo("Hanouj");
+async function toGetRepo() {
+  await Promise.all([
+    toGetUrlRepo('Hailegebriel22'),
+    toGetUrlRepo('patel-prakashkumar'),
+    toGetUrlRepo('Hanouj'),
+  ])
+}
+toGetRepo() 
