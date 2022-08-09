61d8d5c6465d23dc102b06d7cac0fa9709c34ff1
HackYourFuture DK <manisha270989@gmail.com>
2022-08-09 11:08:53 +0200

    updated every 100ms new circle

--- javascript/javascript3/week3/script.js
+++ javascript/javascript3/week3/script.js
@@ -10,7 +10,8 @@ myCtx.fill();
 myCtx.stroke();
 
 //Class creation time!
-const myCanvasCircle = document.getElementById("myCanvasCircle")
+const myCanvasCircle = document.getElementById("myCanvasCircle") //Three different size and radius and color. 
+
 class Circle {
     constructor(x, y, r, startAngle, endAngle, fillColor) {
         this.xPos = x;
@@ -20,8 +21,8 @@ class Circle {
         this.endAngle = endAngle;
         this.fillColor = fillColor;
     }
-    draw() {
-        const ctx = myCanvasCircle.getContext("2d")
+    draw(canvas) {
+        const ctx = canvas.getContext("2d")
         ctx.beginPath();
         ctx.arc(this.xPos, this.yPos, this.radius, this.startAngle, this.endAngle)
         ctx.fillStyle = this.fillColor;
@@ -32,38 +33,20 @@ class Circle {
 const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, "#000000");
 const c2 = new Circle(100, 100, 40, 0, 2 * Math.PI, "#147039");
 const c3 = new Circle(30, 100, 25, 0, 2 * Math.PI, "#047999");
-c1.draw();
-c2.draw();
-c3.draw();
+c1.draw(myCanvasCircle);
+c2.draw(myCanvasCircle);
+c3.draw(myCanvasCircle);
 //Now lets make art!
-const canvasArt = document.getElementById("canvasArt")
-class CanvasArt {
-    constructor(x, y, r, startAngle, endAngle, fillColor) {
-        this.xPos = x;
-        this.yPos = y;
-        this.radius = r;
-        this.startAngle = startAngle;
-        this.endAngle = endAngle;
-        this.fillColor = fillColor;
-    }
-    draw() {
-        const ctxArt = canvasArt.getContext("2d")
-        ctxArt.beginPath();
-        ctxArt.arc(this.xPos, this.yPos, this.radius, this.startAngle, this.endAngle)
-        ctxArt.fillStyle = this.fillColor;
-        ctxArt.fill();
-        ctxArt.stroke
-    }
-}
-//Every 100ms (I made 40ms) create a new circle instance and draw that to the canvas.
-for (let i = 0; i < 300; i++){
-    i += 40;
-    for(let j = 0; j < 200; j++){
-        j += 40
-        const c12 = new CanvasArt(i, j, 10, 0, 2 * Math.PI, "#000000");
-        c12.draw()
-    }
-}
+
+//Every 100ms create a new circle instance and draw that to the canvas.
+const canvasArt = document.getElementById("canvasArt")// 100ms circle.
+let circleAtInterval = setInterval(() => {
+    const c12 = new rCircle();
+    c12.draw(canvasArt)
+}, 100);
+setTimeout(()=>{
+    clearInterval(circleAtInterval)
+}, 10000)
 
 // The circle should have random x, y, radius and color
 const randomCircle = document.getElementById("randomCircle");
@@ -79,8 +62,8 @@ class rCircle {
         this.endAngle = 2 * Math.PI;
         this.fillColor = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0").toUpperCase(); //Random color
     }
-    draw() {
-        const rCircle = randomCircle.getContext("2d")
+    draw(canvas) {
+        const rCircle = canvas.getContext("2d")
         rCircle.beginPath();
         rCircle.arc(this.xPos, this.yPos, this.radius, this.startAngle, this.endAngle)
         rCircle.fillStyle = this.fillColor;
@@ -89,11 +72,11 @@ class rCircle {
     }
 }
 const circle = new rCircle();
-circle.draw()
+circle.draw(randomCircle)
     //when click in the canvas
 randomCircle.addEventListener("click", function() {
     const circle = new rCircle();
-    circle.draw()
+    circle.draw(randomCircle)
 });
 //Follow the mouse - optional
 randomCircle.addEventListener("mousemove", function(e) {
