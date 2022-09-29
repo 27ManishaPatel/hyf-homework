58630dd89522431e3a10ed4df1d866dd5755cf2f
HackYourFuture DK <manisha270989@gmail.com>
2022-09-29 15:23:18 +0200

    preparation work

--- react/react1/week2/class-exercise/src/App.js
+++ react/react1/week2/class-exercise/src/App.js
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
-import './Counter.css'
+import './Counter.css';
+
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className='counter'>
-      <h1>COUNTER</h1>
-      <button onClick={()=>{setCount(count + 1)}}>Click </button>
-      <h3>{count}</h3>
-    </div>
+      <div className='counter'>
+        <h1>COUNTER</h1>
+        <button onClick={() => { setCount(count + 1) }}>Click </button>
+        <h3>{count}</h3>
+      </div>
   );
 }
 
