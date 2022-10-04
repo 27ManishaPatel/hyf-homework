9a2511da1d20ca6b2d1b527278a67defa6e50ddc
HackYourFuture DK <manisha270989@gmail.com>
2022-10-04 10:02:17 +0200

    updated with timer

--- react/react1/week2/to-do-app/src/Header.jsx
+++ react/react1/week2/to-do-app/src/Header.jsx
@@ -1,5 +1,7 @@
+import Timer from './Timer'
 export default function Header () {
     return <header>
         <h1>To-Do List </h1>
+        <Timer/>
     </header>
     }
--- react/react1/week2/to-do-app/src/Main.jsx
+++ react/react1/week2/to-do-app/src/Main.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 
+
 const ToDoLists = [
     {
         id : 1,
