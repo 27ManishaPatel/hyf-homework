808d788d78ab0e893acd4bba0f293517a5602a6d
HackYourFuture DK <manisha270989@gmail.com>
2022-10-10 11:17:02 +0200

    class-exercise

--- react/react1/week3/fetching-data/src/App.js
+++ react/react1/week3/fetching-data/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React ,{useState, useEffect, useCallback} from 'react'
 import './App.css';
 
+  const API_URL = `https://dog.ceo/api/${breed}/image/random`;
+
 function App() {
+  const [src, setSrc] = useState(null)
+  const [breed, setBreed] = useState(null)
+
+  
+  async function fetchDog(){
+    const response = await fetch(API_URL)
+    const {message}= await response.json(breed)
+    setSrc(message)
+  }
+  useEffect(() => {
+    fetchDog()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <input
+    type='text'
+    value={breed}
+    onChange= {(e) => setBreed(e.target.value)} ></input>
+     <button onClick={fetchDog()}>Fetch</button><br></br>
+    {src && < img src={src} width={'50%'} height={'300px'}></img>}
+    </>
   );
 }
 
